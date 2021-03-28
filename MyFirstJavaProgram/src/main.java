f278dbd346e2fbdb4c63bb3145e490156872c7fc
Talent Network <mohammedsedebay@gmail.com>
2021-03-27 21:43:43 -0400

    first commit

---
+++ MyFirstJavaProgram/src/main.java
@@ -0,0 +1,40 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("I love Java");
+		System.out.println("shortcut, type: sysout. than. Contro+spacebar");
+		String Bro_code = "I love your videos";
+		System.out.println(Bro_code);
+		
+		/*
+		 hello this is a comment
+		 */
+		
+		
+		
+		
+		
+		// initialization variable
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = false;       // Boolean
+		String myText = "Hello";
+		System.out.println(myBool);
+		System.out.println(myLetter);
+		System.out.println(myText);
+		System.out.println(myFloatNum);
+		
+		String firstName = "Mohammed ";
+		String lastName = "Sedebay";
+		String fullName = firstName + lastName;
+		System.out.println("Hello, " +fullName);
+		
+
+
+			
+	}
+
+}
